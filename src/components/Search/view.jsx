006f888f79124005773e4d7d30f5978ca2f93041
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2018-02-08 01:34:44 +0800

    add downshift into search

--- src/components/Search/view.jsx
+++ src/components/Search/view.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import Downshift from 'downshift';
+
 import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Icon from 'material-ui/Icon';
 import IconButton from 'material-ui/IconButton';
 import Input, { InputAdornment } from 'material-ui/Input';
+import { MenuItem } from 'material-ui/Menu';
+import Paper from 'material-ui/Paper';
 import Toolbar from 'material-ui/Toolbar';
 
 const styles = themes => ({
@@ -24,6 +28,43 @@ const styles = themes => ({
   }
 });
 
+const suggestions = [
+  { label: 'Afghanistan' },
+  { label: 'Aland Islands' },
+  { label: 'Albania' },
+  { label: 'Algeria' },
+  { label: 'American Samoa' },
+  { label: 'Andorra' },
+  { label: 'Angola' },
+  { label: 'Anguilla' },
+  { label: 'Antarctica' },
+  { label: 'Antigua and Barbuda' },
+  { label: 'Argentina' },
+  { label: 'Armenia' },
+  { label: 'Aruba' },
+  { label: 'Australia' },
+  { label: 'Austria' },
+  { label: 'Azerbaijan' },
+  { label: 'Bahamas' },
+  { label: 'Bahrain' },
+  { label: 'Bangladesh' },
+  { label: 'Barbados' },
+  { label: 'Belarus' },
+  { label: 'Belgium' },
+  { label: 'Belize' },
+  { label: 'Benin' },
+  { label: 'Bermuda' },
+  { label: 'Bhutan' },
+  { label: 'Bolivia, Plurinational State of' },
+  { label: 'Bonaire, Sint Eustatius and Saba' },
+  { label: 'Bosnia and Herzegovina' },
+  { label: 'Botswana' },
+  { label: 'Bouvet Island' },
+  { label: 'Brazil' },
+  { label: 'British Indian Ocean Territory' },
+  { label: 'Brunei Darussalam' }
+];
+
 const BackBtn = () => (
   <InputAdornment position="start">
     <IconButton>
@@ -40,18 +81,88 @@ const SearchBtn = () => (
   </InputAdornment>
 );
 
+const renderTextInput = inputProps => {
+  const { InputProps, ref, ...other } = inputProps;
+
+  return <Input {...other} inputRef={ref} {...InputProps} />;
+};
+
+const renderSuggestion = params => {
+  const { suggestion, index, itemProps, highlightedIndex, selectedItem } = params;
+  const isHighlighted = highlightedIndex === index;
+  const isSelected = selectedItem === suggestion.label;
+
+  return (
+    <MenuItem
+      {...itemProps}
+      key={suggestion.label}
+      selected={isHighlighted}
+      component="div"
+      style={{ fontWeight: isSelected ? 500 : 400 }}
+    >
+      {suggestion.label}
+    </MenuItem>
+  );
+};
+
+const getSuggestion = inputValue => {
+  let count = 0;
+
+  return suggestions.filter(suggestion => {
+    const keep =
+      (!inputValue ||
+        suggestion.label.toLowerCase().includes(inputValue.toLowerCase())) &&
+      count < 5;
+
+    if (keep) {
+      count += 1;
+    }
+
+    return keep;
+  });
+};
+
 export const SearchView = withStyles(styles)(({ classes }) => (
   <div className={classes.root}>
     <AppBar position="static" color="default">
-      <Toolbar>
-        <Input
-          fullWidth
-          disableUnderline
-          placeholder="Search"
-          startAdornment={<BackBtn />}
-          endAdornment={<SearchBtn />}
-        />
-      </Toolbar>
+      <Downshift itemToString={item => (item === null ? '' : item.label)}>
+        {({
+          getInputProps,
+          getItemProps,
+          isOpen,
+          inputValue,
+          selectedItem,
+          highlightedIndex
+        }) => (
+          <div>
+            <Toolbar>
+              {renderTextInput({
+                fullWidth: true,
+                disableUnderline: true,
+                InputProps: getInputProps({
+                  placeholder: 'Search',
+                  id: 'searchInput'
+                }),
+                startAdornment: <BackBtn />,
+                endAdornment: <SearchBtn />
+              })}
+            </Toolbar>
+            {isOpen ? (
+              <Paper square>
+                {getSuggestion(inputValue).map((suggestion, index) =>
+                  renderSuggestion({
+                    suggestion,
+                    index,
+                    itemProps: getItemProps({ item: suggestion.label }),
+                    highlightedIndex,
+                    selectedItem
+                  })
+                )}
+              </Paper>
+            ) : null}
+          </div>
+        )}
+      </Downshift>
     </AppBar>
     Search goes here
   </div>
