800f10e0efce98e59875242b736cbd1012ee03b5
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2018-03-31 23:06:37 +0800

    enable touch

--- src/components/HandWriteCanvas.jsx
+++ src/components/HandWriteCanvas.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import { canvasToBlob } from '../utils/promise-helper';
 
+const createEventListener = canvas => e => {
+  if (e.target === canvas) {
+    e.preventDefault();
+  }
+};
+
+const setUpForCanvas = eventListener => {
+  // Prevent scrolling when touching the canvas
+  // document.body.addEventListener('touchstart', eventListener, false);
+  // document.body.addEventListener('touchend', eventListener, false);
+  // document.body.addEventListener('touchmove', eventListener, false);
+  document.body.style.touchAction = 'none';
+};
+
+const cleanUpCanvas = eventListener => {
+  // document.body.removeEventListener('touchstart', eventListener, false);
+  // document.body.removeEventListener('touchend', eventListener, false);
+  // document.body.removeEventListener('touchmove', eventListener, false);
+  document.body.style.touchAction = null;
+};
+
 class HandWriteCanvasContainer extends React.Component {
   canvasRef = React.createRef();
   ctx = null;
+  eventListener = null;
   lastX = 0;
   lastY = 0;
 
@@ -11,8 +33,23 @@ class HandWriteCanvasContainer extends React.Component {
     isDrawing: false
   };
 
+  extractOffSetFromEvent = e => {
+    const { offsetX, offsetY, touches } = e.nativeEvent;
+    if (offsetX && offsetY) {
+      return { offsetX, offsetY };
+    }
+    const rect = this.canvasRef.getBoundingClientRect();
+    const x = touches[0].clientX - rect.left;
+    const y = touches[0].clientY - rect.top;
+
+    return {
+      offsetX: x,
+      offsetY: y
+    };
+  };
+
   handleMouseDown = e => {
-    const { offsetX, offsetY } = e.nativeEvent;
+    const { offsetX, offsetY } = this.extractOffSetFromEvent(e);
     this.lastX = offsetX;
     this.lastY = offsetY;
 
@@ -24,7 +61,7 @@ class HandWriteCanvasContainer extends React.Component {
   handleMouseMove = e => {
     const { color } = this.props;
     if (this.state.isDrawing) {
-      const { offsetX, offsetY } = e.nativeEvent;
+      const { offsetX, offsetY } = this.extractOffSetFromEvent(e);
       const ctx = this.ctx;
       ctx.strokeStyle = color || '#000';
       const lastX = this.lastX;
@@ -52,6 +89,8 @@ class HandWriteCanvasContainer extends React.Component {
   };
 
   componentDidMount() {
+    this.eventListener = createEventListener(this.canvasRef);
+    setUpForCanvas(this.eventListener);
     this.canvasRef.width = window.innerWidth - 50;
     this.canvasRef.height = window.innerHeight - 100;
     this.ctx = this.canvasRef.getContext('2d');
@@ -61,13 +100,20 @@ class HandWriteCanvasContainer extends React.Component {
     this.ctx.lineCap = 'round';
   }
 
+  componentWillUnmount() {
+    cleanUpCanvas(this.eventListener);
+  }
+
   render() {
     const { render } = this.props;
     const canvasNode = (
       <canvas
         onMouseDown={this.handleMouseDown}
+        onTouchStart={this.handleMouseDown}
         onMouseMove={this.handleMouseMove}
+        onTouchMove={this.handleMouseMove}
         onMouseUp={this.handleMouseUp}
+        onTouchEnd={this.handleMouseUp}
         ref={ref => (this.canvasRef = ref)}
       />
     );
